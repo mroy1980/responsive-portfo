924f8848595039f395615a2194f9b712abed60b9
M Roy <mroy.in21@gmail.com>
2022-12-14 15:23:04 +0530

    app modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-    <h1 className="text-center">This is my App component</h1>
+    <h1 >This is my App component</h1>
     
     
     </>
